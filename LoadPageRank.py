6a512a7b6254c397a5d88a4784c173cae7deeed5
charnugagoo <charnugagoo.gmail.com>
2013-03-24 23:48:47 -0400

    LoadPageRank.py

---
+++ LoadPageRank.py
@@ -0,0 +1,21 @@
+from getPageRank import getPageRank, getAlexaRank
+path = "/Users/charnugagoo/Documents/Workspace/InvertedIndexLargeDataSet/LargeDateset/"
+file = open(path + "DocMetaData_large_set_with_pageRank.txt", 'w')
+cnt = 0
+for line in open(path + "DocMetaData_large_set.txt"):
+    print cnt
+    cnt += 1
+    w = line.split()
+    if len(w) == 1:
+        file.write(w[0])
+    elif len(w) == 4:
+        file.write(w[0] + " ")
+        file.write(w[1] + " ")
+        file.write(w[2] + " ")
+        pr = float(getPageRank(w[1]))
+        ar = float(getAlexaRank(w[1]))
+        file.write(str(pr) + " ")
+        file.write(str(ar) + "\n")
+    else:
+        continue
+file.close()
